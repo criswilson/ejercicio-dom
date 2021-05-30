da457e6e76ee558995e88b15a3652732fdb1f43c
cristian <cristian.wilson3@gmail.com>
2021-05-30 03:30:32 -0300

    primer commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const button = document.getElementById("meetCris");
+const h1 = document.getElementsByClassName("name");
+const h2 = document.getElementsByTagName("h2");
+
+console.log(button);
+console.log(h1);
+console.log(h2);
